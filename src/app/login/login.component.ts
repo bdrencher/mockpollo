8735ebb46c703973b5dadc55cf330300fb0ae269
bdrencher <bdrencher@gmail.com>
2020-12-19 15:24:59 -0700

    adding login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  username = new FormControl();
+  password = new FormControl();
+  formControls = {
+    "username": this.username,
+    "password": this.password
+  }
+
+  loginFormGroup = new FormGroup(this.formControls, [Validators.required]);
+
+  constructor(private router: Router) { }
+
+  verifyCredentials(): void {
+    if (this.password.value == '' || this.username.value == '') {
+      this.fireValidation();
+    } else {
+      this.router.navigate(['dashboard']);
+    }
+  }
+
+  fireValidation() {
+    alert("invalid credentials");
+  }
 
   ngOnInit(): void {
   }
