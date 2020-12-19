bf07967919372492be8f48caa2762bb030cbd9c3
bdrencher <bdrencher@gmail.com>
2020-12-19 15:39:54 -0700

    adding create account page

--- src/app/create-account/create-account.component.ts
+++ src/app/create-account/create-account.component.ts
@@ -1,4 +1,7 @@
+import { Route } from '@angular/compiler/src/core';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-account',
@@ -7,8 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateAccountComponent implements OnInit {
 
-  constructor() { }
+  username = new FormControl();
+  firstPass = new FormControl();
+  secondPass = new FormControl();
+  formControls = {
+    "username": this.username,
+    "firstPass": this.firstPass,
+    "secondPass": this.secondPass,
+  };
 
+  createAccountFormGroup = new FormGroup(this.formControls, [Validators.required]);
+
+  constructor(private router: Router) { }
+
+  createAccount() {
+    if (this.firstPass.value != this.secondPass.value || this.firstPass.value == '') {
+      alert("The passwords do not match or are invalid, please enter a different password");
+    } else {
+      this.router.navigate(['dashboard']);
+    }
+  }
+  
   ngOnInit(): void {
   }
 
